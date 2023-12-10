dbb73e33156ae33d4327bccb9d14baa0964bce46
oblakooorta <152392860+oblakooorta@users.noreply.github.com>
2023-12-10 17:36:24 +0300

    Update Guidance.py

--- Guidance.py
+++ Guidance.py
@@ -1,19 +1,20 @@
 import pygame
-
-from Ball import Ball
+import math 
 """
 Это своего рода прицел - стрелка, которая показывает направление прыжка шарика.
 Чем длиннее стрелка, тем с большей скоростью полетит шарик
 """
+RED = 0xFF0000
+
 
 class Guidance: 
-    def __init__(self, screen, x, y):
+    def __init__(self, screen, x, y, obj):
         self.screen = screen
         self.f2_power = 10
         self.f2_on = False
         self.an = 1
-        self.x = ball.x
-        self.y = ball.y
+        self.x = obj.x
+        self.y = obj.y
         self.color = RED
         
     def fire2_start(self, event, obj):
@@ -33,9 +34,9 @@ class Guidance:
             if self.f2_power < 50:
                 self.f2_power += 0.5
                 
-    def update(self):
-        self.x = ball.x
-        self.y = ball.y
+    def update(self, obj):
+        self.x = obj.x
+        self.y = obj.y
         
     def draw(self):
         pygame.draw.line(
